922a7232c6a87130358e9b20dfadca175ceb6355
tjmaynes-indebted <tj.maynes@indebted.co>
2022-08-01 16:10:20 -0500

    Single shareble install.sh script

--- go2/scripts/install.sh
+++ go2/scripts/install.sh
@@ -3,7 +3,10 @@
 set -o pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
-source "$ROOT_DIR/scripts/shared.sh"
+
+COLOR_RED='\033[0;31m'
+COLOR_DARK_GRAY='\033[1;30m'
+COLOR_NONE='\033[0m' # No Color
 
 function check_requirements() {
   if [[ ! -f "$ROOT_DIR/.tool-versions" ]]; then
@@ -12,6 +15,33 @@ function check_requirements() {
   elif [[ -z "$(command -v asdf)" ]]; then
     echo "Please install 'asdf' before running this script"
     exit 1
+  fi
+}
+
+function os_name() {
+  OS=`uname -s`
+}
+
+function _log_info() {
+  echo "${COLOR_DARK_GRAY}${@}${COLOR_NONE}"
+}
+
+function _log_error() {
+  echo "${COLOR_RED}${@}${COLOR_NONE}"
+}
+
+function _backup_file() {
+  now=$(date +'%Y-%m-%d-%H-%M-%S')
+
+  mkdir -p ${HOME}/.onboarding
+
+  original=$1
+  filename=$(basename $original)
+
+  if [ -f $original ]; then
+    backup="${HOME}/.onboarding/$filename-$now"
+    cp "$original" "$backup"
+    _log_info "Backed up original $original to $backup";
   fi
 }
 
